fcb3e71fafd60fcca3f86ed3c8cf431936db6b6e
Devesh Avutu <devesh14may@gmail.com>
2020-05-19 11:03:55 +0530

    created login query

--- server/resolvers/user.js
+++ server/resolvers/user.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
 	createUser: async ({ userInput: { email, password } }) => {
@@ -19,5 +20,23 @@ module.exports = {
 		} catch (err) {
 			throw err;
 		}
+	},
+	login: async ({ email, password }) => {
+		const user = await User.findOne({ email });
+		if (!user) {
+			throw new Error("User doesn't exist");
+		}
+		const isEqual = await bcrypt.compare(password, user._doc.password);
+		if (!isEqual) {
+			throw new Error("Invalid Credentials");
+		}
+		const token = jwt.sign(
+			{ userId: user._doc._id, email: user._doc.email },
+			process.env.TOKEN_SECRET,
+			{
+				expiresIn: "1h",
+			}
+		);
+		return { userId: user._doc._id, token, tokenExpiration: 1 };
 	},
 };
